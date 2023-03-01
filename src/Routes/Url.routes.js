096f00b487a19963a227bbe364eb13a7594f5152
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-03-01 13:51:15 -0300

    fix: url schema and createdAt columns

--- src/Routes/Url.routes.js
+++ src/Routes/Url.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { DELETEUrl, GETUrlById, OpenUrl, POSTUrl } from "../Controllers/Url.controller.js";
 import { AuthMiddleware } from "../Middleware/Auth.middleware.js";
+import SchemaMiddleware from "../Middleware/Schema.middleware.js";
 import { DELETEUrlMiddleware } from "../Middleware/Url.middleware.js";
+import UrlSchema from "../Schemas/Url.schema.js";
 
 const urlRouter = Router();
 
-urlRouter.post('/urls/shorten', AuthMiddleware, POSTUrl);
+urlRouter.post('/urls/shorten', SchemaMiddleware(UrlSchema), AuthMiddleware, POSTUrl);
 urlRouter.get('/urls/:id', GETUrlById);
 urlRouter.get('/urls/open/:shortUrl', OpenUrl);
 urlRouter.delete('/urls/:id', AuthMiddleware, DELETEUrlMiddleware, DELETEUrl);
